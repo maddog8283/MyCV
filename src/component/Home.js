fa650ef7a75c1c86507288b4d43cad8210aea4d7
Dnlmzdjcb <rasdmywmm@gmail.com>
2025-07-03 01:46:56 +0800

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,42 +1,8 @@
-// import React, { Component } from "react";
-// import Education from "./Education";
-// import Portofolios from "./Portofolios";
-// import Experience from "./Experience";
-// import Skils from "./Skils";
-// import Profile from "./Profile";
-// import Navbar from "./Navbar";
-// import About from "./About";
-
-// export default class Home extends Component {
-//   render() {
-//     return (
-//       <section>
-//         <Navbar />
-//         <div className="container">
-//           <div className="row">
-//             <div classname="col s12 m4 l3">
-//               <Profile />
-//               <About />
-//             </div>
-//             <div className="col s12 m8 l9">
-//               <Skils />
-//               <Experience />
-//               <Education />
-//               <Portofolios />
-//             </div>
-//           </div>
-//         </div>
-//       </section>
-//     );
-//   }
-// }
-
 import React, { Component } from "react";
 import Education from "./Education";
 import Experience from "./Experience";
 import Skills from "./Skils";
 import Profile from "./Profile";
-import Navbar from "./Navbar";
 import About from "./About";
 import Hobby from "./Hobby";
 
@@ -44,7 +10,6 @@ export default class Home extends Component {
   render() {
     return (
       <section>
-        <Navbar />
         <div className="container mt-4">
           <div className="row">
             {/* Sidebar kiri */}
